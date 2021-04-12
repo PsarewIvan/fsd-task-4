f6a512baad000bdc4f0d9e2600d693d4b65eb081
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-12 13:28:21 +0300

    Разбивает демо страницу на компоненты

--- demo/index.ts
+++ demo/index.ts
@@ -1,44 +1,65 @@
 import $ from 'jquery';
-import Slider from './pug/includes/slider/Slider';
+// import Slider from './pug/includes/slider/Slider';
 import './styles/global.scss';
 import './pug/pages/index.scss';
 import '../src';
 
-const Slider1 = new Slider($('.js-slider-1'), {
-  min: 300,
-  max: 500,
-  values: [400],
-  step: 0.1,
-  scale: true,
-  scaleMark: 10,
-  subScaleMark: 5,
+const slidersOptions: Partial<Settings>[] = [
+  {
+    min: 300,
+    max: 500,
+    values: [400],
+    step: 0.1,
+    scale: true,
+    scaleMark: 10,
+    subScaleMark: 5,
+  },
+  {
+    type: 'range',
+    min: -100,
+    max: 300,
+    values: [30, 100],
+    step: 10,
+    scale: true,
+  },
+  {
+    orientation: 'vertical',
+    min: 0,
+    max: 100,
+    values: [30],
+    step: 1,
+    scale: true,
+    hints: false,
+    tooltips: false,
+  },
+  {
+    orientation: 'vertical',
+    type: 'range',
+    min: -100,
+    max: 100,
+    values: [0, 50],
+    step: 5,
+  },
+];
+
+$('.js-slider').each(function (index) {
+  $(this).freeSlider(slidersOptions[index]);
 });
 
-const Slider2 = new Slider($('.js-slider-2'), {
-  type: 'range',
-  min: -100,
-  max: 300,
-  values: [30, 100],
-  step: 10,
-  scale: true,
-});
-
-const SLider3 = new Slider($('.js-slider-3'), {
-  orientation: 'vertical',
-  min: 0,
-  max: 100,
-  values: [30],
-  step: 1,
-  scale: true,
-  hints: false,
-  tooltips: false,
-});
-
-const Slider4 = new Slider($('.js-slider-4'), {
-  orientation: 'vertical',
-  type: 'range',
-  min: -100,
-  max: 100,
-  values: [0, 50],
-  step: 5,
-});
+interface Settings {
+  min: number;
+  max: number;
+  step: number;
+  values: Array<number>;
+  percents: Array<number>;
+  type: 'single' | 'range';
+  orientation: 'horizontal' | 'vertical';
+  scale: boolean;
+  scaleMark: number;
+  subScaleMark: number;
+  tooltips: boolean;
+  hints: boolean;
+  onChange: (value: Array<number>) => void;
+  onFinish: (value: Array<number>) => void;
+  onUpdate: (value: Array<number>) => void;
+}
